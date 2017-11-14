12df04d22d75f3936bd83737891ddca36a70a085
Unknown <rducros34@gmail.com>
2017-11-14 14:53:45 +0100

    Vibra + Notif plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -42,6 +42,41 @@ module.exports = [
             "cordova.plugins.backgroundMode",
             "plugin.backgroundMode"
         ]
+    },
+    {
+        "id": "cordova-plugin-local-notification.LocalNotification",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+        "pluginId": "cordova-plugin-local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local"
+        ]
+    },
+    {
+        "id": "cordova-plugin-local-notification.LocalNotification.Core",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+        "pluginId": "cordova-plugin-local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "id": "cordova-plugin-local-notification.LocalNotification.Util",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+        "pluginId": "cordova-plugin-local-notification",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "id": "cordova-plugin-vibration.notification",
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -51,7 +86,9 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-nativestorage": "2.2.2",
     "cordova-plugin-badge": "0.8.5",
-    "cordova-plugin-background-mode": "0.7.2"
+    "cordova-plugin-background-mode": "0.7.2",
+    "cordova-plugin-local-notification": "0.9.0-beta.1",
+    "cordova-plugin-vibration": "2.1.6"
 };
 // BOTTOM OF METADATA
 });
