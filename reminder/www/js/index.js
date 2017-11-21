6b2ca77c30404a93a550c4183c7128630d615e75
Unknown <rducros34@gmail.com>
2017-11-21 14:33:52 +0100

    Merge branch 'master' into TP_Android_Reminder/master

--- reminder/www/js/index.js
+++ reminder/www/js/index.js
@@ -1,36 +1,136 @@
 var app = {
 
-    //valeur du compteur
-    compteur: 0,
-
+    cpt: 0,
     // Application Constructor
     initialize: function () {
+        this.bindEvents();
         if (window.cordova) {
-            //        Cas ou on est sur un peripherique cordova
-            console.log("Device detected ! ");
             document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
         } else {
-            //        Cas ou on est sur un navigateur
             this.onDeviceReady();
-            console.log("Browser detected ! ");
+            console.log("navigateur")
         }
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+//    bindEvents: function () {
+//        document.addEventListener('deviceready', this.onDeviceReady, false);
+//    },
+
+//    setDeviceReady: function () {
+//        $("#Online").show();
+//        $("#Offline").hide();
+//    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    start: function () {
+        $("#cpt").html("OK!");
+        setInterval(function () {
+            this.cpt++;
+            this.updateDom();
+        }.bind(this), 1000);
+    },
+
+    updateDom: function () {
+        $("#cpt").html(this.cpt);
+    },
+
+    updateClock: function () {
+        var d = new Date();
+        $("#hourClock").html(d.getHours());
+        $("#minClock").html(d.getMinutes());
+        $("#secClock").html(d.getSeconds());
+
+        var dayOfWeek = days[d.getDay()];
+        var month = months[d.getMonth()];
+        var day = d.getDate();
+        var year = d.getFullYear();
+        var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
+        
+        $("#date").html(dateString);
     },
 
     onDeviceReady: function () {
+
+        app.receivedEvent('modalReminder');
+
+        var interval = 5000;
+        console.log(window.cordova);
+
+        //Mise en tache de fond de l'application.
+        window.cordova.plugins.backgroundMode.enable();
+
+        setInterval(function () {
+            if (cordova.plugins.backgroundMode.isActive()) {
+                console.log("Le mode background est actif !");
+                app.checkAlarm();
+                interval = 15000;
+            } else {
+                console.log("Le mode background est inactif !");
+                app.updateClock();
+                interval = 1000;
+            }
+            console.log("Update since : " + interval + " ms")
+        }, interval);
+
+        cordova.plugins.backgroundMode.overrideBackButton();
+        cordova.plugins.backgroundMode.excludeFromTaskList();
+
+        //console.log(device.cordova);
         this.setDeviceReady();
         this.start();
+
     },
 
-    setDeviceReady: function () {
-        $("#offline").hide();
-        $("#online").show();
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+        document.getElementById("btn_load").onclick = app.loadData;
+        document.getElementById("btn_save").onclick = app.saveData;
     },
 
-    start: function () {
-        $("#compteur").html("OK!");
-        setInterval(function () {
-            $("#compteur").html(app.compteur++);
-        }, 1000);
+    saveData: function (storageKey) {
+        
+        var dataStored = JSON.parse(loadData(storageKey));
+        var alarmName = $("#newRemindText").val();
+        var alarmTime = $("#newRemindTime").val();
+        
+        var string = '{"items":[{"name":"'+alarmName+'"},{"time":"'+alarmTime+'"}]}';
+        
+        NativeStorage.set(storageKey,
+            JSON.stringify(string),
+            function (result) {
+                alert("Saved Data : " + result);
+            },
+            function (e) {
+                fail("Object Writing Failed");
+            });
+    },
+
+    loadData: function (storageKey) {
+        NativeStorage.getString(storageKey,
+            function (result) {
+                alert("Current Stored Value was: " + result);
+                var parsedObject = JSON.parse(result);
+            },
+            function (e) {
+                fail("Read Object Failed");
+            });
+    },
+
+    checkAlarm: function () {
+
     }
 };
 
